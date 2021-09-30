4aec3ffbefcf514ca4862c21558159f48c9ce4ce
cyphics <cyphics@protonmail.com>
2021-09-30 10:27:22 +0200

    initial commit

---
+++ app/src/main/java/ch/sooon/billing/BaseActivity.kt
@@ -0,0 +1,28 @@
+/** ========================================================================
+Creation: 25/02/21
+Revision: $
+Creator: Thierry Raeber (thierry@sooon.ch)
+Notice: (C) Copyright 2021 by Sooon SA. All Rights Reserved.
+============================================================================ */
+
+package ch.sooon.billing
+
+import android.app.Activity
+import android.os.Bundle
+import ch.sooon.billing.MyWatchFace.Companion.billingDataSource
+import ch.sooon.billing.databinding.BaseActivityBinding
+
+class BaseActivity : Activity() {
+	private val TAG = this.javaClass.simpleName
+
+	private lateinit var binding: BaseActivityBinding
+
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		binding = BaseActivityBinding.inflate(layoutInflater)
+		val view = binding.root
+		setContentView(view)
+       	billingDataSource?.launchBillingFlow(this, "billing_premium")
+	}
+
+}
